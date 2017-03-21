48e5cc7af683bd87e3240a8cb19a90d50cd632c4
Divyanshu Maithani <div.blackcat@gmail.com>
2017-03-21 22:17:44 +0530

    :guitar: Set up Unsplash Source ES6

---
+++ src/index.js
@@ -0,0 +1,10 @@
+class UnsplashSourceES6 {
+  constructor() {
+    this._name = 'UnsplashSourceES6';
+  }
+  get name() {
+    return this._name;
+  }
+}
+
+export default UnsplashSourceES6;
---
+++ test/library.spec.js
@@ -0,0 +1,21 @@
+/* global describe, it, before */
+
+import chai from 'chai';
+import UnsplashSourceES6 from '../lib/unsplash-source-es.js';
+
+chai.expect();
+
+const expect = chai.expect;
+
+let lib;
+
+describe('Given an instance of my library', () => {
+  before(() => {
+    lib = new UnsplashSourceES6();
+  });
+  describe('when I need the name', () => {
+    it('should return the name', () => {
+      expect(lib.name).to.be.equal('UnsplashSourceES6');
+    });
+  });
+});
