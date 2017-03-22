eb0c16090e3ac66d7ac89637279a70484c926219
Divyanshu Maithani <div.blackcat@gmail.com>
2017-03-22 23:28:03 +0530

    More :coffee: Mocha and :tea: Chai

--- test/library.spec.js
+++ test/library.spec.js
@@ -2,7 +2,7 @@
 // TODO: Add Tests
 
 import chai from 'chai';
-import UnsplashSourceES6 from '../lib/unsplash-source-es.js';
+import UnsplashSourceES6 from '../lib/unsplash-source-es6.js';
 
 chai.expect();
 
@@ -10,13 +10,21 @@ const expect = chai.expect;
 
 let lib;
 
-describe('Given an instance of my library', () => {
-  before(() => {
+describe('Given an instance of unsplash-source-es6', () => {
+
+  beforeEach(() => {
     lib = new UnsplashSourceES6();
   });
-  describe('when I need the name', () => {
-    it('should return the name', () => {
-      expect(lib.name).to.be.equal('UnsplashSourceES6');
+
+  describe('when called getURL() only', () => {
+    it('should return the random URL', () => {
+      expect(lib.getURL()).to.be.equal('https://source.unsplash.com/random');
+    });
+  });
+
+  describe('when called id("some-id").getURL()', () => {
+    it('should return the URL for the same photo id', () => {
+      expect(lib.id('some-id').getURL()).to.be.equal('https://source.unsplash.com/some-id');
     });
   });
 });
