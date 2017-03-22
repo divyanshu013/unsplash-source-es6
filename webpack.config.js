c6540c353ef72b9a189498a243fa41a033640e84
Divyanshu Maithani <div.blackcat@gmail.com>
2017-03-22 23:25:26 +0530

    Update library name in Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
 const env = require('yargs').argv.env; // use --env with webpack 2
 
-let libraryName = 'unsplash-source-es';
+let libraryName = 'unsplash-source-es6';
 
 let plugins = [], outputFile;
 
