40bd375ff88273b1d4383d3c5a4265fcc80b7c34
Divyanshu Maithani <div.blackcat@gmail.com>
2017-03-22 23:27:36 +0530

    Add size() and id() wrappers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 class UnsplashSourceES6 {
   constructor() {
     this.url = 'https://source.unsplash.com';
+    this.category;
+    this.user;  // user and likes
+    this.collection;
+    this.weekly;
+    this.daily;
+    this.search;  // ?term or ?term1,term2
+    this.specifiphoto;
+  }
+
+  // get Image by its ID
+  id(photoId) {
+    this.photoId = photoId;
+
+    return this;
+  }
+
+  // sets the photo dimension, if only one parameter is given, height is taken same as width
+  size(width, height = width) {
+    this.dimension.width = width;
+    this.dimension.height = height;
+
+    return this;
+  }
+
+  // add this.dimension (dimensions) to the url
+  _addDimensions() {
+    if (this.hasOwnProperty('dimension')) {
+      this.url += '/' + this.dimension.width + 'x' + this.dimension.height;
+    }
+
+    return this;
   }
 
   // get the Image URL
   getURL() {
+    if (typeof this.photoId !== 'undefined') {
+      this.url += '/' + this.photoId;
+    }
+    else {
+      this.url += '/random';
+    }
+    this._addDimensions();
     return this.url;
   }
 }
