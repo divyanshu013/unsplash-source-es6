2d350c738e0f34c83c38b5dd3c5049879516b929
Divyanshu Maithani <div.blackcat@gmail.com>
2017-03-25 22:10:34 +0530

    Add tests for size() API

--- test/library.spec.js
+++ test/library.spec.js
@@ -22,9 +22,27 @@ describe('Given an instance of unsplash-source-es6', () => {
     });
   });
 
+  describe('when called size(1980, 1080).getURL()', () => {
+    it('should return the random URL with added resolution', () => {
+      expect(lib.size(1980, 1080).getURL()).to.be.equal('https://source.unsplash.com/random/1980x1080');
+    });
+  });
+
   describe('when called id("some-id").getURL()', () => {
     it('should return the URL for the same photo id', () => {
       expect(lib.id('some-id').getURL()).to.be.equal('https://source.unsplash.com/some-id');
+    });
+  });
+
+  describe('when called id("some-id").size(1980, 1080).getURL()', () => {
+    it('should return the URL for the same photo id with added resolution', () => {
+      expect(lib.id('some-id').size(1980, 1080).getURL()).to.be.equal('https://source.unsplash.com/some-id/1980x1080');
+    });
+  });
+
+  describe('when called id("some-id").size(1980).getURL()', () => {
+    it('should return the URL for the same photo id with added resolution and same height as width', () => {
+      expect(lib.id('some-id').size(1980).getURL()).to.be.equal('https://source.unsplash.com/some-id/1980x1980');
     });
   });
 });
